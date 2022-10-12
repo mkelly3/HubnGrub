9cc2bb36fafa75dc9a99f7bafeae0553fa7fa7e2
Morgan Kelly <mkelly5380@gmail.com>
2022-10-11 19:23:56 -0500

    add APIs for tasty, spotify and itunes

---
+++ script.js
@@ -0,0 +1,53 @@
+//Gi
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': 'd9d33242b0msh33204813b478dddp15d47ajsne912a7d2c892',
+// 		'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=celery', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+
+
+//look up recipes based off a single ingrident
+
+const specification = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd9d33242b0msh33204813b478dddp15d47ajsne912a7d2c892',
+		'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+	}
+};
+
+fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes&q=chicken', specification)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+
+
+
+//spotify playlist 
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'd9d33242b0msh33204813b478dddp15d47ajsne912a7d2c892',
+      'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+    }
+  };
+  
+  fetch('https://spotify23.p.rapidapi.com/playlist/?id=37i9dQZF1DX4Wsb4d7NKfP', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+
+    //pick your favorite artist 
+    fetch("https://itunes.apple.com/search?term=jack+johnson&limit=25")
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));

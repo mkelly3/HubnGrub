bc7d0fba52b13e633b078da580566df3cedf511b
Angela-Amaro <aamaro@alumnae.mills.edu>
2022-10-13 16:49:30 -0500

    added a new api not complete

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var optionsEl = document.querySelector('.option');
 var dishElementEl = document.querySelector('.dishElement');
 var musicEl = document.querySelector('.musicChoice');
 var cookingTimeEl = document.querySelector('.cookingTime');
-var resultsEl = document.querySelector("#recipeResult");
+var resultsEl = document.querySelector("#food");
 function onStartButton() {
       $('.startBtn').click(function(){
             homeCardEl.setAttribute("data-style","hide");
@@ -14,123 +14,92 @@ function onStartButton() {
 onStartButton();
 function getRecipes(ingrident,time){
       //console.log(ingrident, time);
-      if(time ==="Easy Meals Under 30 minutes"){
-             const specification = {
-            method: 'GET',
-            headers: {
-                  'X-RapidAPI-Key': 'd9d33242b0msh33204813b478dddp15d47ajsne912a7d2c892',
-                  'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-            }
-      };
-      fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes&q='+ingrident, specification)
+      // if(time ==="Easy Meals Under 30 minutes"){
+        
+      // };
+      fetch("https://api.edamam.com/api/recipes/v2?q=chicken&app_id=c4b76db4&app_key=8e4555653962229a6f8dc77e94e23d4e")
       .then(function (response) {
             return response.json();
             })
             .then(function (response) {
             console.log(response);
-            resultsEl.textContent="";
-            var cardDiv = document.createElement('div');
-            var titleResult = document.createElement('h3');
-            var resultImg = document.createElement('img');
-            //cardDiv.append(imgEl);
-            var dishName = [];
-            for(var i=0; i < 10; i++){
-             dishName[i] = response.results[i].name;
-             }
-             console.log(dishName);
-            for(var i =0; i<dishName.length; i++){
-                  titleResult.textContent = String(dishName);
-            }
-            cardDiv.appendChild(titleResult);
-            resultsEl.appendChild(cardDiv);
-            resultsEl.removeAttribute("data-style");
-             var dishImgUrl = [];
-             for(var i=0; i < 10; i++){
-                  dishImgUrl[i] = response.results[i].thumbnail_url;
-                 }
-                  console.log(dishImgUrl);
-            for(var i=0; i<dishImgUrl; i++){
-                  resultImg[i].src = dishImgUrl[i];
-            }
-            cardDiv.append(resultImg);
+            //resultsEl.textContent="";
+            // var cardDiv = document.createElement('div');
+            // var titleResult = document.createElement('h3');
+            // var resultImg = document.createElement('img');
+            // //cardDiv.append(imgEl);
+            // var dishName = [];
+            // for(var i=0; i < 10; i++){
+            //  if (response.results[i].name){
+            //       dishName[i]=response.results[i].name
+            //  }
+            //  }
+            //  console.log(dishName);
+            // for(var i =0; i<dishName.length; i++){
+            //       titleResult.textContent = dishName;
+            // }
+            // cardDiv.appendChild(titleResult);
+            // resultsEl.appendChild(cardDiv);
+            // resultsEl.removeAttribute("data-style");
+            //  var dishImgUrl = [];
+            //  for(var i=0; i < 10; i++){
+            //       dishImgUrl[i] = response.results[i].thumbnail_url;
+            //      }
+            //       console.log(dishImgUrl);
+            // for(var i=0; i<dishImgUrl; i++){
+            //       resultImg[i].src = dishImgUrl[i];
+            // }
+            // cardDiv.append(resultImg);
             })
-            .catch(err => console.error(err));
-      }
-
-            console.log(data);
-            var dishName = []
             
-            for(var i=0; i < 10; i++){
-             dishName[i] = response.results[i].name;
-            }
-             console.log(dishName);
-             dishName=dishName.filter(item=>item);
-             for (i=0; i<dishName.length; i++){
-                   $(".name"+i).text(dishName[i])
-             }
-             var dishImgUrl = []
-
-             for(var i=0; i < 10; i++){
-                  dishImgUrl[i] = response.results[i].thumbnail_url;
-                 }
-                  console.log(dishImgUrl);
-                  
-            var nutrition = []
-
-            for(var i=0; i < 10; i++){
-                  nutrition[i] = response.results[i].nutrition;
-                 }
-                  console.log(nutrition);
-
-            })
             .catch(err => console.error(err));
 
       }
 
-      else{
-            console.log('yes');
-            const specification = {
-           method: 'GET',
-           headers: {
-                 'X-RapidAPI-Key': 'd9d33242b0msh33204813b478dddp15d47ajsne912a7d2c892',
-                 'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
-           }
-     };
+//       else{
+//             console.log('yes');
+//             const specification = {
+//            method: 'GET',
+//            headers: {
+//                  'X-RapidAPI-Key': 'd9d33242b0msh33204813b478dddp15d47ajsne912a7d2c892',
+//                  'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+//            }
+//      };
      
-     fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&q='+ingrident, specification)
-     .then(function (response) {
-      return response.json();
-      })
+//      fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&q='+ingrident, specification)
+//      .then(function (response) {
+//       return response.json();
+//       })
 
-      .then(function (response) {
+//       .then(function (response) {
       
-       //document.location.href ="recipe.html";
-       console.log(response)
-       var dishName = []
+//        //document.location.href ="recipe.html";
+//        console.log(response)
+//        var dishName = []
             
-       for(var i=0; i < 10; i++){
-            dishName[i] = response.results[i].name;
-           }
-            console.log(dishName);
+//        for(var i=0; i < 10; i++){
+//             dishName[i] = response.results[i].name;
+//            }
+//             console.log(dishName);
 
-            var dishImgUrl = []
+//             var dishImgUrl = []
 
-            for(var i=0; i < 10; i++){
-                 dishImgUrl[i] = response.results[i].thumbnail_url;
-                }
-                 console.log(dishImgUrl);
+//             for(var i=0; i < 10; i++){
+//                  dishImgUrl[i] = response.results[i].thumbnail_url;
+//                 }
+//                  console.log(dishImgUrl);
                  
-           var nutrition = []
+//            var nutrition = []
 
-           for(var i=0; i < 10; i++){
-                 nutrition[i] = response.results[i].nutrition;
-                }
-                 console.log(nutrition);
-      })
-           .catch(err => console.error(err)); 
-      }
+//            for(var i=0; i < 10; i++){
+//                  nutrition[i] = response.results[i].nutrition;
+//                 }
+//                  console.log(nutrition);
+//       })
+//            .catch(err => console.error(err)); 
+//       }
       
-}
+// }
 
 function getMusic(music){
       //search for an artisit based on genre 

48ec546408afed0dfc66136d185552c766236e57
Morgan Kelly <mkelly5380@gmail.com>
2022-10-13 07:50:13 -0500

    select the recipe options

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
 
+
+var homeCardEl = document.querySelector(".homeCard");
+var optionsEl = document.querySelector('.option');
+var dishElementEl = document.querySelector('.dishElement');
+var servingEl = document.querySelector('.servingSizeElement');
+var cookingTimeEl = document.querySelector('.cookingTime');
+
 //look up recipes based off a single ingrident
+
+
+
+
+
+
+
+    //pick your favorite artist 
+    //place the artist you want or type of music and it displays a list of URL's and music videos
+    fetch("https://itunes.apple.com/search?term=Billie+Ellish&limit=25")
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+  
+
+function onStartButton() {
+      $('.startBtn').click(function(){
+            homeCardEl.setAttribute("data-style","hide");
+            optionsEl.removeAttribute("data-style","hide");
+      })
+
+};
+
+onStartButton();
+
+// console.log(dishElementEl.options[dishElementEl.selectedIndex].text);
+onSearchButton();
+
+
+function getRecipes(ingrident,time,number){
+
+}
 
 const specification = {
 	method: 'GET',
@@ -15,30 +55,24 @@ fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_mi
 	.catch(err => console.error(err));
 
 
-
-
-
-    //pick your favorite artist 
-    //place the artist you want or type of music and it displays a list of URL's and music videos
-    fetch("https://itunes.apple.com/search?term=Billie+Ellish&limit=25")
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
-
-  
-
-var homeCardEl = document.querySelector(".homeCard");
-var optionsEl = document.querySelector('.option');
-
-function onStartButton() {
-      $('.startBtn').click(function(){
-            homeCardEl.setAttribute("data-style","hide");
-            optionsEl.removeAttribute("data-style","hide");
-
-
-      })
-
-};
-
-onStartButton();
-
+      function onSearchButton(){
+            $('.searchBtn').click(function(event){
+                  event.preventDefault();
+      
+                  if(dishElementEl.selectedIndex === 0 || cookingTimeEl.selectedIndex === 0 || 
+                        servingEl.selectedIndex === 0){
+                              return;
+                        }
+      
+                  var dishChoice = dishElementEl.options[dishElementEl.selectedIndex].text;
+                  console.log(dishChoice);
+                  
+                  var timeChoice = cookingTimeEl.options[cookingTimeEl.selectedIndex].text;
+                  console.log(timeChoice);
+      
+                  var numberChoice = servingEl.options[servingEl.selectedIndex].text;
+                  console.log(numberChoice);  
+      
+                   getRecipes(dishChoice,timeChoice,numberChoice);
+            })
+      }

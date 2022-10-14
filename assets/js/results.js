0894dd5be147e95bb323178006d87eef2b96c54b
Angela-Amaro <aamaro@alumnae.mills.edu>
2022-10-14 15:44:33 -0500

    added text to display when no search history is found when recent HubnGrubs is clicked

--- assets/js/results.js
+++ assets/js/results.js
@@ -1,2 +1,14 @@
 var recentSearches= JSON.parse(localStorage.getItem("savedCardStringify"));
 console.log(recentSearches)
+var mainEl = document.querySelector(".main");
+
+function displayCards () {
+    if (!recentSearches) {
+    var noSearch;
+    noSearch = document.createElement("h3");
+    noSearch.textContent= ("You have no recent search history!");
+    mainEl.appendChild(noSearch);
+    }
+    
+}
+displayCards()

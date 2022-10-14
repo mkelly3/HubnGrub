a41a543cdb2c8ff5c250fe04710bb587f55d3f7a
Angela-Amaro <aamaro@alumnae.mills.edu>
2022-10-14 16:00:11 -0500

    got recent search to display in the center of the page

--- assets/js/results.js
+++ assets/js/results.js
@@ -9,6 +9,18 @@ function displayCards () {
     noSearch.textContent= ("You have no recent search history!");
     mainEl.appendChild(noSearch);
     }
-    
+    for (var i=0; i<recentSearches.artists.length; i++) {
+        var savedDiv = document.createElement("div");
+        var Meal = document.createElement("h2");
+        var song = document.createElement("h3");
+        var artist = document.createElement("h3");
+        savedDiv.appendChild(Meal);
+        savedDiv.appendChild(song);
+        savedDiv.appendChild(artist);
+        Meal.textContent = recentSearches.titleRecipe[i];
+        song.textContent = recentSearches.songTitle[i];
+        artist.textContent = recentSearches.artists[i];
+        mainEl.appendChild(savedDiv);
+    }
 }
 displayCards()
